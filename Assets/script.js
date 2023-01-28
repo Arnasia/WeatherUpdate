67b93909a0d834cce1a4059debaf2559138e2002
Arnasia Jackson <arnasial.jackson@gmail.com>
2023-01-28 17:16:04 -0500

    add weather icon incurrent weather function

--- Assets/script.js
+++ Assets/script.js
@@ -5,6 +5,7 @@ let currentWeather = document.getElementById("today-weather");
 let currentTemp = document.getElementById("temperature");
 let currentHumidity = document.getElementById("humidity");
 let currentWind = document.getElementById("wind-speed");
+let currentIconEl = document.getElementById("current-pic");
 
 let APIKey = "84b79da5e5d7c92085660485702f4ce8";
 
@@ -20,6 +21,11 @@ function theWeather(cityName) {
         let month = currentDate.getMonth() + 1;
         let year = currentDate.getFullYear();
         searchedCity.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+        
+        let weatherIcon = response.data.weather[0].icon;
+        currentIconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
+        currentIconEl.setAttribute("alt", response.data.weather[0].description);
+
         currentTemp.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
         currentHumidity.innerHTML = "Humidity: " + response.data.main.humidity + "%";
         currentWind.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
@@ -28,9 +34,9 @@ function theWeather(cityName) {
 
 }
 
-function k2f(K) {
-        return Math.floor((K - 273.15) * 1.8 + 32);
-    }
+    function k2f(K) {
+            return Math.floor((K - 273.15) * 1.8 + 32);
+        }
 
     searchBtn.addEventListener("click", function () {
         let citySearch = city.value;
