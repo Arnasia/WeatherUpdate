eed6eb901fd2604cd79784b2467203415a4c4901
Arnasia Jackson <arnasial.jackson@gmail.com>
2023-01-28 16:15:46 -0500

    search button works and deploys city name and current date

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,9 @@
-let APIKey = "84b79da5e5d7c92085660485702f4ce8";
-let currentWeather = document.getElementById("today-weather")
+let city = document.getElementById("enter-city");
+let searchBtn = document.getElementById("search-button");
+let searchedCity = document.getElementById("city-name");
+let currentWeather = document.getElementById("today-weather");
 
+let APIKey = "84b79da5e5d7c92085660485702f4ce8";
 
 function theWeather(cityName) {
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
@@ -9,4 +12,21 @@ function theWeather(cityName) {
 
         currentWeather.classList.remove("d-none");
 
+        let currentDate = new Date(response.data.dt * 1000);
+        let day = currentDate.getDate();
+        let month = currentDate.getMonth() + 1;
+        let year = currentDate.getFullYear();
+        searchedCity.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+        
+      })    
+
 }
+
+    searchBtn.addEventListener("click", function () {
+        let citySearch = city.value;
+        theWeather(citySearch);
+        searchHistory.push(citySearch);
+        
+    })
+
+    
