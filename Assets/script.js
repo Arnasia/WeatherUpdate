3d4387565c2951724de6c52fcc814d23b1bb1033
Arnasia Jackson <arnasial.jackson@gmail.com>
2023-01-28 17:07:06 -0500

    added functionality to retrieve temperature, humidity, wind and display it in html

--- Assets/script.js
+++ Assets/script.js
@@ -2,6 +2,9 @@ let city = document.getElementById("enter-city");
 let searchBtn = document.getElementById("search-button");
 let searchedCity = document.getElementById("city-name");
 let currentWeather = document.getElementById("today-weather");
+let currentTemp = document.getElementById("temperature");
+let currentHumidity = document.getElementById("humidity");
+let currentWind = document.getElementById("wind-speed");
 
 let APIKey = "84b79da5e5d7c92085660485702f4ce8";
 
@@ -17,10 +20,17 @@ function theWeather(cityName) {
         let month = currentDate.getMonth() + 1;
         let year = currentDate.getFullYear();
         searchedCity.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+        currentTemp.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
+        currentHumidity.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+        currentWind.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
         
       })    
 
 }
+
+function k2f(K) {
+        return Math.floor((K - 273.15) * 1.8 + 32);
+    }
 
     searchBtn.addEventListener("click", function () {
         let citySearch = city.value;
